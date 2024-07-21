ae2a189421ba847986cdf306c91a102b19cc9def
Bruce101github <brucethiombiano@gmail.com>
2024-07-21 13:18:37 +0000

    added a methon to check if an equation is one-step or multi-step

--- equation.py
+++ equation.py
@@ -1,1 +1,25 @@
+from operation import Operation
+
+
+class Equation(Operation):
+    """Handles and checks all equations"""
+
+    def __init__(self):
+        "Initializing"
+        super().__init__()
+
+    def equation_type(self, equation):
+        """Checks if an equation is one step or multi-step"""
+        signs = 0
+        for sign in equation:
+            if (sign == self.addition or
+                sign == self.subtraction or 
+                sign == self.multiplication or 
+                sign == self.division or
+                sign == self.remainder):
+                signs += 1
+        if signs == 1:
+            return "One-step"
+        elif signs > 1:
+            return "Multi-step"
 

341ecbe7700a2cfd96df33fcf59097c56a20ae7d
Bruce101github <brucethiombiano@gmail.com>
2024-07-22 14:15:34 +0000

    calculator can now calculate both one step and multi step equations

--- calculator.py
+++ calculator.py
@@ -12,7 +12,30 @@ while active:
         answer = calculator.solve(digits, sign)
         print(answer)
     elif type == "Multi-step":
-        print("This app only supports one step equations for now")
+        signs = []
+        for sign in equation:
+            if (sign == "+" or
+                sign == "-" or
+                sign == "x" or
+                sign == "/" or 
+                sign == "%"):
+                signs.append(sign)
+        digits = []
+        for sign in signs:
+            x = equation.split(sign, 1)
+            if ("+" in x[-1] or
+                "-" in x[-1] or
+                "x" in x[-1] or
+                "/" in x[-1] or
+                "%" in x[-1]):    
+                equation = x[-1]
+                y = x.pop(0)
+                digits.append(y)
+            else:
+                while x:
+                    for i in x:
+                        digits.append(x.pop(0))
+        print(calculator.solve_multi_step(digits,signs))
     elif equation == "off":
         break
 
